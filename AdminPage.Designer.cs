05aafa36f6bd8a4047e53ebe7e33ba67735a4047
Omar Tamer <omardaihom@gmail.com>
2025-04-27 14:27:54 +0300

    admn tables created
    admin tables created

---
+++ AdminPage.Designer.cs
@@ -0,0 +1,85 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace E_Commerce_Store
+{
+    partial class AdminPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminPage));
+            titleLabel = new Label();
+            dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            resources.ApplyResources(titleLabel, "titleLabel");
+            titleLabel.ForeColor = Color.FromArgb(241, 250, 238);
+            titleLabel.Name = "titleLabel";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(dataGridView1, "dataGridView1");
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { resources.GetString("comboBox1.Items"), resources.GetString("comboBox1.Items1"), resources.GetString("comboBox1.Items2"), resources.GetString("comboBox1.Items3"), resources.GetString("comboBox1.Items4") });
+            resources.ApplyResources(comboBox1, "comboBox1");
+            comboBox1.Name = "comboBox1";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // AdminPage
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(29, 53, 87);
+            Controls.Add(comboBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(titleLabel);
+            Name = "AdminPage";
+            Load += AdminPage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button loginButton;
+        private TextBox passwordTextBox;
+        private Label titleLabel;
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ AdminPage.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace E_Commerce_Store;
+
+internal partial class AdminPage : Form
+{
+    DataTable users = new DataTable();
+    DataTable sellers = new DataTable();
+    DataTable products = new DataTable();
+    DataTable categories = new DataTable();
+    DataTable orders = new DataTable();
+    internal AdminPage() => InitializeComponent();
+
+    private void searchForProductsTextBox_TextChanged(object sender, EventArgs e)
+    {
+        // Implement logic that refreshes the dataview control with products that match the input text
+    }
+
+    private void AdminPage_Load(object sender, EventArgs e)
+    {
+        comboBox1.SelectedIndex = 0;
+        // Users table (based on "Customer")
+        users.Columns.Add("Customer_ID");
+        users.Columns.Add("FName");
+        users.Columns.Add("LName");
+        users.Columns.Add("Phone_Number");
+        users.Columns.Add("DOB");
+        users.Columns.Add("Email");
+
+        // Sellers table
+        sellers.Columns.Add("Seller_ID");
+        sellers.Columns.Add("FName");
+        sellers.Columns.Add("LName");
+        sellers.Columns.Add("Phone_Number");
+        sellers.Columns.Add("Email");
+        sellers.Columns.Add("Address");
+
+        // Products table
+        products.Columns.Add("Product_ID");
+        products.Columns.Add("Description");
+        products.Columns.Add("Status");
+        products.Columns.Add("Published_At");
+        products.Columns.Add("Price");
+        products.Columns.Add("Seller_ID");    // Foreign Key
+        products.Columns.Add("Category_Name");  // Foreign Key
+
+        // Orders table
+        orders.Columns.Add("Order_ID");
+        orders.Columns.Add("Date");
+        orders.Columns.Add("Shipping_Address");
+        orders.Columns.Add("Status");
+        orders.Columns.Add("Customer_ID"); // Foreign Key
+        orders.Columns.Add("Billing_Address");
+
+        // Categories table
+        categories.Columns.Add("Category_ID");
+        categories.Columns.Add("Name");
+        categories.Columns.Add("Description");
+    }
+
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (comboBox1.SelectedIndex == 0)
+        {
+            //users sellers products orders categs
+            dataGridView1.DataSource = users;
+        }
+        else if (comboBox1.SelectedIndex == 1)
+        {
+            dataGridView1.DataSource = sellers;
+        }
+        else if (comboBox1.SelectedIndex == 2)
+        {
+            dataGridView1.DataSource = products;
+        }
+        else if (comboBox1.SelectedIndex == 3)
+        {
+            dataGridView1.DataSource = orders;
+        }
+        else if (comboBox1.SelectedIndex == 4)
+        {
+            dataGridView1.DataSource = categories;
+        }
+
+        dataGridView1.Update();
+    }
+
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+
+    }
+}
