9804bb58b3766c86fc3b3b8d67f37c4eb01e4e48
Adam <23P0001@eng.asu.edu.eg>
2025-05-01 09:36:07 +0300

    Check if user has already added this card before.

--- Customer/Add Payment.cs
+++ Customer/Add Payment.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Windows.Forms;
 using E_Commerce_Store.Common;
 
@@ -28,20 +29,24 @@ internal partial class AddPayment : Form
 
         try
         {
-            var query = $"""
-                         
-                                             INSERT INTO Payment (Customer_ID, Card_Number, CardHolder_Name, Card_Expiry_Date)
-                                             VALUES ({Login.User.Value}, '{cardNumber}', '{cardholderName}', '{expiryDate:yyyy-MM-dd}')
-                                         
-                         """;
+            var checkQuery = $"SELECT 1 FROM Payment WHERE Customer_ID = {Login.User.Value} AND Card_Number = '{cardNumber}'";
 
+            if (Program.DatabaseHandler.ExecuteQuery(checkQuery) is DataTable { Rows.Count: > 0 })
+            {
+                MessageBox.Show("You have already added this card.", "E-Commerce Store", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
-            Program.DatabaseHandler.ExecuteQuery(query);
+            var insertQuery = $"""
+                                   INSERT INTO Payment (Customer_ID, Card_Number, CardHolder_Name, Card_Expiry_Date)
+                                   VALUES ({Login.User.Value}, '{cardNumber}', '{cardholderName}', '{expiryDate:yyyy-MM-dd}')
+                               """;
+
+            Program.DatabaseHandler.ExecuteQuery(insertQuery);
 
             MessageBox.Show("Payment added successfully!", "E-Commerce Store", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             _myProfile.MyProfile_Load(null, null);
-
             Close();
         }
         catch (Exception ex)
@@ -49,4 +54,5 @@ internal partial class AddPayment : Form
             MessageBox.Show($"Failed to add payment. Error: {ex.Message}", "E-Commerce Store", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
+
 }
